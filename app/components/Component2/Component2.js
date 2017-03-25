755b0d6984af2b80f485a571001641b57e3c3ea6
ismailunal <ismailunal@gmail.com>
2017-03-25 23:34:35 +0000

    flex boxes

---
+++ app/components/Component2/Component2.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View, StyleSheet} from 'react-native';
+
+export default class Component2 extends Component {
+  render() {
+    return(
+      <View>
+        <View style={styles.myView}>
+          <Text style={styles.myText}>
+            Hello Ali
+          </Text>
+        </View>
+          <View style={styles.container}>
+            <View style={styles.v1}>
+              <Text>
+              View 1
+              </Text>
+            </View>
+            <View style={styles.v2}>
+              <Text>
+              View 2
+              </Text>
+            </View>
+            <View style={styles.v3}>
+              <Text>
+              View 3
+              </Text>
+            </View>
+          </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  myView: {
+    margin:'10%', backgroundColor:'#ddd'
+  },
+  myText: {
+    color: 'red'
+  },
+  container: {
+    flexDirection:'row',
+    height:100
+  },
+  v1: {
+    flex:1,
+    backgroundColor:'red',
+    padding:10
+  },
+  v2: {
+    flex:1,
+    backgroundColor:'blue',
+    padding:10
+  },
+  v3: {
+    flex:1,
+    backgroundColor:'green',
+    padding:10
+  }
+});
+
+AppRegistry.registerComponent('Component2', () => Component2)
