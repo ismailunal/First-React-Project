da1b8354610c894a5c7f3a2ae519e75ac9a62d2a
ismailunal <ismailunal@gmail.com>
2017-03-26 23:11:28 +0100

    Navigator example

---
+++ app/components/Component5/Component5.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View, ListView, StyleSheet, TouchableHighlight} from 'react-native';
+
+export default class Component5 extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    this.state = {
+      userDataSource: ds,
+    };
+  }
+
+componentDidMount(){
+  this.fetchusers();
+}
+
+  fetchusers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((response) => {
+          this.setState({
+            userDataSource: this.state.userDataSource.cloneWithRows(response)
+          });
+      });
+  }
+
+onPress(user) {
+  this.props.navigator.push({
+    id: 'component6',
+    user: user
+  })
+}
+
+  renderRow(user, sectionId, rowId, highlightRow) {
+    return(
+      <TouchableHighlight onPress={() => {this.onPress(user)}}>
+        <View style={styles.row}>
+          <Text style={styles.rowText}>
+            {user.name} - {user.email}
+          </Text>
+        </View>
+      </TouchableHighlight>
+  )
+  }
+
+  render() {
+    return(
+      <View>
+        <ListView
+          dataSource={this.state.userDataSource}
+          renderRow={this.renderRow.bind(this)}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding:'10%',
+    backgroundColor: '#f4f4f4',
+    marginBottom:3
+
+  }
+});
+
+AppRegistry.registerComponent('Component5', () => Component5)
--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {AppRegistry, Text, View, Navigator} from 'react-native';
 
-import Component4 from './app/components/Component4/Component4'
+import Component5 from './app/components/Component5/Component5'
+import Component6 from './app/components/Component6/Component6'
 
 export default class ReactFirstProject extends Component {
+
+  renderScene(route, navigator) {
+    switch (route.id) {
+      case 'component5':
+        return (<Component5 navigator={navigator} title="component5" />)
+      case 'component6':
+          return (<Component6 user={route.user} navigator={navigator} title="component6" />)
+    }
+  }
+
   render() {
     return(
-      <View>
-        <Component4/>
-      </View>
+      <Navigator
+        initialRoute={{id: 'component5'}}
+        renderScene={this.renderScene}
+        configureScreen={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+      />
     );
   }
 }
